64771033983370d19d30c55ed8917f3250484665
andreas.josef.wagner <andreas.josef.wagner@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-07-13 22:48:04 +0000

    Fixed both wizards. Should be working fine now ...

--- xxplore_eclipse/src/org/aifb/xxplore/wizards/DataSourceWizard.java
+++ xxplore_eclipse/src/org/aifb/xxplore/wizards/DataSourceWizard.java
@@ -1,20 +1,40 @@
 package org.aifb.xxplore.wizards;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.aifb.xxplore.core.ExploreEnvironment;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.ide.IDE;
+import org.xmedia.oms.model.api.IOntology;
+import org.xmedia.oms.persistence.KbEnvironment;
 
 public class DataSourceWizard extends Wizard implements INewWizard {
-	protected IStructuredSelection m_selection;
-	protected IWorkbench m_workbench;
-	protected DataSourceWizardMainPage m_mainpage;
+	
+	private IStructuredSelection m_selection;
+	private IWorkbench m_workbench;
+	private DataSourceWizardMainPage m_mainpage;
+	
 	
 	public DataSourceWizard() {
 		super();
@@ -22,8 +42,14 @@ public class DataSourceWizard extends Wizard implements INewWizard {
 	
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		m_workbench = workbench;
+		m_selection = selection;
 		setWindowTitle("New Data Source");
 		setNeedsProgressMonitor(true);
+	}
+	
+	@Override
+	public boolean isHelpAvailable() {
+		return false;
 	}
 	
 	public IWorkbench getWorkbench() {
@@ -36,31 +62,300 @@ public class DataSourceWizard extends Wizard implements INewWizard {
 	
 	@Override
 	public void addPages() {
-		m_mainpage = new DataSourceWizardMainPage(getSelection());
-		m_mainpage.setTitle("New Ontology Datasource");
-		m_mainpage.setDescription("Create a new file representing parameters for a Datasource");
+		m_mainpage = new DataSourceWizardMainPage();
+		addPage(m_mainpage);
 	}
 
 	@Override
 	public boolean performFinish() {
-		IFile file = m_mainpage.createNewFile();
-		if (file == null){
+
+		HashMap<String, String> props = m_mainpage.getSelectedProperties();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(props.get(ExploreEnvironment.PROJECT_NAME));
+		
+		try {
+			IFile datasource = WizardHelper.createDataSourceFile(project, props, null);
+			project.setPersistentProperty(WizardHelper.DATASOURCE, datasource.getFullPath().toPortableString());
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		catch (CoreException e){
+			e.printStackTrace();
 			return false;
 		}
 		
-		IWorkbenchWindow dw = getWorkbench().getActiveWorkbenchWindow();
-		try {
-			if (dw !=null){
-				IWorkbenchPage page = dw.getActivePage();
-				if (page != null)
-					IDE.openEditor(page,file, true);
-			}
-		} catch (PartInitException e) {
-			//TODO what should we do with the drunken error.....!!!!
-			e.printStackTrace();
-		}
-		
-		return false;
+		return true;
 	}
 	
+	
+	public class DataSourceWizardMainPage extends WizardPage {
+		
+		private Text m_datasource_name_text;
+		private Text m_repository_name_text;
+		private Text m_datasourceUri_text;
+		private Text m_baseUri_text;
+		
+		private Combo m_combo_syntax;
+		private Combo m_combo_expressivity;
+		private Combo m_combo_project;
+		
+		private Label m_syntax_label;
+		private Label m_repository_name_label;
+		private Label m_baseUri_label;
+		
+		private static final String TITLE = "New Ontology Datasource";
+		private static final String DESC = "Create a new file representing a XXPlore-Datasource";
+		
+		private final String[] m_syntax = {IOntology.TRIX_LANGUAGE, IOntology.RDF_XML_LANGUAGE, IOntology.N3_LANGUAGE};		
+		private final String[] m_expressivity = {IOntology.OWL_EXPRESSIVITY, IOntology.RDFS_EXPRESSIVITY};
+		private Composite m_composite;
+		
+		public DataSourceWizardMainPage() {
+			super(TITLE);
+			super.setDescription(DESC);
+		}
+		
+		public void createControl(Composite parent) {
+			
+			m_composite = new Composite(parent, SWT.NONE);
+			GridData gridData = new GridData(SWT.FILL, SWT.None, true, false);
+			String[] projects;
+			GridLayout layout = new GridLayout(2, false);
+			m_composite.setLayout(layout);
+			
+			Label m_project_label = new Label(m_composite, SWT.NONE);
+			m_project_label.setText("Project");
+			m_combo_project = new Combo(m_composite, SWT.DROP_DOWN);
+			m_combo_project.setLayoutData(gridData);
+			m_combo_project.setItems(projects = getProjects());
+			m_combo_project.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			Label m_datasource_name_label = new Label(m_composite, SWT.NONE);
+			m_datasource_name_label.setText("Datasource Name");
+			
+			m_datasource_name_text = new Text(m_composite, SWT.BORDER);
+			m_datasource_name_text.setText("datasource");
+			m_datasource_name_text.setLayoutData(gridData);
+			m_datasource_name_text.setToolTipText("Please enter a name for your new datasource file.");
+			m_datasource_name_text.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			Label urilabel = new Label(m_composite, SWT.NONE);
+			urilabel.setText("Ontology Location");
+			m_datasourceUri_text = new Text(m_composite, SWT.BORDER);
+			m_datasourceUri_text.setLayoutData(gridData);
+			m_datasourceUri_text.setTextLimit(500);
+			m_datasourceUri_text.setToolTipText("Enter the location of the knowledge base you want to explore");
+			m_datasourceUri_text.setText("c:/explore.owl");
+			m_datasourceUri_text.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			Label expressivity_label = new Label(m_composite, SWT.NONE);
+			expressivity_label.setText("Language");
+			m_combo_expressivity = new Combo(m_composite, SWT.DROP_DOWN);
+			m_combo_expressivity.setLayoutData(gridData);
+			m_combo_expressivity.setItems(m_expressivity);
+			m_combo_expressivity.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+					updateButtons();
+				}
+			});
+			
+			m_syntax_label = new Label(m_composite, SWT.NONE);
+			m_syntax_label.setText("Syntax");
+			m_combo_syntax = new Combo(m_composite, SWT.DROP_DOWN);
+			m_combo_syntax.setLayoutData(gridData);
+			m_combo_syntax.setItems(m_syntax);
+			m_combo_syntax.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			m_syntax_label.setEnabled(false);
+			m_combo_syntax.setEnabled(false);
+			
+			m_repository_name_label = new Label(m_composite, SWT.NONE);
+			m_repository_name_label.setText("Repository Name");
+			m_repository_name_text = new Text(m_composite, SWT.BORDER | SWT.FILL);
+			m_repository_name_text.setText("My_repository");
+			m_repository_name_text.setToolTipText("Please enter a name for your repository.");
+			m_repository_name_text.setLayoutData(gridData);
+			m_repository_name_text.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			m_repository_name_label.setEnabled(false);
+			m_repository_name_text.setEnabled(false);
+			
+			m_baseUri_label = new Label(m_composite, SWT.NONE);
+			m_baseUri_label.setText("Base URI");
+			m_baseUri_text = new Text(m_composite, SWT.BORDER | SWT.FILL);
+			m_baseUri_text.setToolTipText("Please provide your ontology base uri - if none, leave blank.");
+			m_baseUri_text.setLayoutData(gridData);
+			m_baseUri_text.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					getContainer().updateButtons();
+				}
+			});
+			
+			m_baseUri_text.setEnabled(false);
+			m_baseUri_text.setEnabled(false);
+						
+			setControl(m_composite);
+			
+			if(projects.length == 0){
+				noProjectError();
+			}
+		}
+		
+		private void updateButtons(){			
+			if(m_combo_expressivity.getText().equals(IOntology.OWL_EXPRESSIVITY)){
+				m_syntax_label.setEnabled(false);
+				m_combo_syntax.setEnabled(false);
+				m_repository_name_label.setEnabled(false);
+				m_repository_name_text.setEnabled(false);
+				m_baseUri_label.setEnabled(false);
+				m_baseUri_text.setEnabled(false);
+			}
+			else if(m_combo_expressivity.getText().equals(IOntology.RDFS_EXPRESSIVITY)){
+				m_syntax_label.setEnabled(true);
+				m_combo_syntax.setEnabled(true);
+				m_repository_name_label.setEnabled(true);
+				m_repository_name_text.setEnabled(true);
+				m_baseUri_label.setEnabled(true);
+				m_baseUri_text.setEnabled(true);
+			}
+		}
+				
+		public HashMap<String,String> getSelectedProperties(){
+			
+			HashMap<String,String> properties = new HashMap<String, String>();
+			properties.put(ExploreEnvironment.PROJECT_NAME, m_combo_project.getText().trim());
+			properties.put(ExploreEnvironment.DATASOURCE_FILENAME, m_datasource_name_text.getText().trim());
+			properties.put(ExploreEnvironment.ONTOLOGY_EXPRESSITIVITY, m_combo_expressivity.getText().trim());
+			
+			if(m_combo_expressivity.getText().equals(IOntology.OWL_EXPRESSIVITY)){
+				properties.put(KbEnvironment.PHYSICAL_ONTOLOGY_URI, m_datasourceUri_text.getText().trim());			
+			}
+			else if(m_combo_expressivity.getText().equals(IOntology.RDFS_EXPRESSIVITY)){
+				
+				properties.put(ExploreEnvironment.REPOSITORY_NAME, m_repository_name_text.getText().trim());
+				properties.put(ExploreEnvironment.ONTOLOGY_FILE_PATH, m_datasourceUri_text.getText().trim());
+				properties.put(ExploreEnvironment.ONTOLOGY_FILE_NAME, m_datasourceUri_text.getText().trim());
+				properties.put(ExploreEnvironment.BASE_ONTOLOGY_URI, m_baseUri_text.getText().trim());
+				properties.put(ExploreEnvironment.LANGUAGE, m_combo_syntax.getText().trim());
+				
+			}
+			
+			return properties;
+		}
+				
+		@Override
+		public boolean isPageComplete() {
+			
+			String error = new String();
+			boolean is_valid = true;
+			
+//			check whether all fields are set
+			
+			if((m_combo_project.getText().trim() == null) || m_combo_project.getText().trim().equals("")){
+				error = "Project not set. Please provide a valid project.";
+				is_valid = false;
+			}
+			else if((m_datasource_name_text.getText().trim() == null) || m_datasource_name_text.getText().trim().equals("")){
+				error = "Please provide a name for your datasource.";
+				is_valid = false;
+			}
+			else if((m_combo_expressivity.getText().trim() == null) || m_combo_expressivity.getText().trim().equals("")){
+				error = "Please provide the used expressivity.";
+				is_valid = false;
+			}
+			else if(m_combo_expressivity.getText().equals(IOntology.OWL_EXPRESSIVITY)){				
+				
+				if((m_datasourceUri_text.getText().trim() == null) || m_datasourceUri_text.getText().trim().equals("")){
+					error = "Please provide the location of your knowledge base.";
+					is_valid = false;
+				}
+			}
+			else if(m_combo_expressivity.getText().equals(IOntology.RDFS_EXPRESSIVITY)){
+				
+				if((m_repository_name_text.getText().trim() == null) || m_repository_name_text.getText().trim().equals("")){
+					error = "Please provide a name for your repository.";
+					is_valid = false;
+				}
+				else if((m_datasourceUri_text.getText().trim() == null) || m_datasourceUri_text.getText().trim().equals("")){
+					error = "Please provide the location of your knowledge base.";
+					is_valid = false;
+				}
+//				Note, the base-Uri may be empty!
+				else if((m_combo_syntax.getText().trim() == null) || m_combo_syntax.getText().trim().equals("")){
+					error = "Please provide the syntax used in your knowledge base.";
+					is_valid = false;
+				}
+			}
+
+			
+//			check whether knowledge base exists
+			
+			String filepath = m_datasourceUri_text.getText();
+			File file = new File(filepath); 					
+			if(!file.exists()){
+				error = "Knowledge base not found, using path '"+filepath+"'.";
+				is_valid = false;
+			}
+			
+			if(!is_valid){
+				setErrorMessage(error);
+				return false;
+			}
+			else{
+				setErrorMessage(null);
+				return true;
+			}
+		}
+		
+		private String[] getProjects(){
+			
+			int i = 0;
+			IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+			
+			String[] project_names = new String[projects.length];
+			
+			for(IProject project : projects){
+				project_names[i++] = project.getName();
+			}
+						
+			return project_names;
+		}
+		
+		private void noProjectError(){
+			
+			String title = "No Project Error";
+			String message = "Currently your workspace contains on project.\nBefore creating a datasource file, please create a project.";
+
+			MessageBox mb = new MessageBox(m_composite.getShell(), SWT.ICON_WORKING | SWT.OK);
+			mb.setText(title);
+			mb.setMessage(message);
+
+            if (mb.open() == SWT.OK) {
+            	((WizardDialog)getContainer()).close();
+            	return;
+            }			
+		}
+	}
 }
---
+++ xxplore_eclipse/src/org/aifb/xxplore/wizards/WizardHelper.java
@@ -0,0 +1,110 @@
+package org.aifb.xxplore.wizards;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.aifb.xxplore.core.ExploreEnvironment;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.QualifiedName;
+import org.xmedia.accessknow.sesame.persistence.SesameRepositoryFactory;
+import org.xmedia.oms.model.api.IOntology;
+import org.xmedia.oms.persistence.KbEnvironment;
+
+public class WizardHelper {
+
+	public static final QualifiedName DATASOURCE = new QualifiedName(ExploreEnvironment.NAME_QUALIFIER, ExploreEnvironment.DATASOURCE_LOCALNAME);
+	
+	/**
+	 * The method creates a datasource file (usually with an extension "ods") in the project
+	 * given as parameter. The datasource URI given in parameter is the uri of the datasource 
+	 * (perhaps a non initialised Datasource object could be given as parameter).
+	 * 
+	 * @param project - the name of the project into which the datasource is stored.
+	 * @param file_name - the name of the file 
+	 * @param datasourceURI - the URI of the data source (N.B: this may change later on)
+	 * @param monitor - the listener monitoring the progress of the file creation
+	 * @return the IFile resource created
+	 * @throws CoreException - A core exception is thrown if ...
+	 * @throws IOException - An IO Exception is thrown if  the content can not be written correctly into the file
+	 */
+	public static IFile createDataSourceFile(IProject project, HashMap<String,String> properties, IProgressMonitor monitor) 
+		throws CoreException, IOException {
+		
+		IPath path = new Path(project.getLocation().getDevice(),properties.get(ExploreEnvironment.DATASOURCE_FILENAME)+ExploreEnvironment.DATASOURCE_EXTENSION);
+		IFile datasource = project.getFile(path);
+		InputStream in = null;
+		
+		try {		
+			StringBuffer buff = new StringBuffer();
+					
+			if(properties.get(ExploreEnvironment.ONTOLOGY_EXPRESSITIVITY).equals(IOntology.OWL_EXPRESSIVITY)){
+				
+				buff.append(KbEnvironment.PHYSICAL_ONTOLOGY_URI+"=");
+				buff.append(properties.get(KbEnvironment.PHYSICAL_ONTOLOGY_URI));
+				buff.append("\n");
+				
+				buff.append(KbEnvironment.CONNECTION_PROVIDER_CLASS+"="+
+						"org.xmedia.oms.adapter.kaon2.persistence.Kaon2ConnectionProvider\n");
+				
+				buff.append("session.factory_class=" +
+						"org.xmedia.oms.adapter.kaon2.persistence.Kaon2SessionFactory\n");
+				
+			}
+			else if(properties.get(ExploreEnvironment.ONTOLOGY_EXPRESSITIVITY).equals(IOntology.RDFS_EXPRESSIVITY)){
+				
+				buff.append(ExploreEnvironment.REPOSITORY_NAME+"=");
+				buff.append(properties.get(ExploreEnvironment.REPOSITORY_NAME));
+				buff.append("\n");
+				
+				buff.append(ExploreEnvironment.ONTOLOGY_FILE_PATH+"=");
+				buff.append(properties.get(ExploreEnvironment.ONTOLOGY_FILE_PATH));
+				buff.append("\n");
+				
+				buff.append(ExploreEnvironment.ONTOLOGY_FILE_NAME+"=");
+				buff.append(properties.get(ExploreEnvironment.ONTOLOGY_FILE_NAME));
+				buff.append("\n");
+				
+				buff.append(ExploreEnvironment.BASE_ONTOLOGY_URI+"=");
+				buff.append(properties.get(ExploreEnvironment.BASE_ONTOLOGY_URI));
+				buff.append("\n");
+				
+				buff.append(ExploreEnvironment.LANGUAGE+"=");
+				buff.append(properties.get(ExploreEnvironment.LANGUAGE));
+				buff.append("\n");
+				
+				buff.append(KbEnvironment.ONTOLOGY_TYPE+"=");
+				buff.append(SesameRepositoryFactory.RDFS_MEMORY_PERSISTENT);
+				buff.append("\n");
+				
+				buff.append(KbEnvironment.CONNECTION_PROVIDER_CLASS+"="+
+						"org.xmedia.accessknow.sesame.persistence.ConnectionProvider\n");
+				
+				buff.append("session.factory_class="+
+						"org.xmedia.accessknow.sesame.persistence.SesameSessionFactory\n");
+				
+			}
+			
+			buff.append(KbEnvironment.REASONER_ON+"=false");
+			
+			in = new ByteArrayInputStream(buff.toString().getBytes());
+			if(datasource.exists()){
+				datasource.setContents(in, false, true, monitor);
+			} else {
+				datasource.create(in, false, monitor);
+			}
+		} 
+		finally {
+			if(in != null){
+				in.close();
+			}
+		}
+		return datasource;
+	}
+}
