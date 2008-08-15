3dc69f84dce707d7fd305f5a86c5c91f86a686df
andreas.josef.wagner <andreas.josef.wagner@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-08-15 21:04:30 +0000

    Bug fix

--- trunk/xxplore_eclipse/src/org/aifb/xxplore/XXPloreCommandline.java
+++ trunk/xxplore_eclipse/src/org/aifb/xxplore/XXPloreCommandline.java
@@ -18,12 +18,11 @@ import org.aifb.xxplore.shared.util.PropertyUtils;
 import org.aifb.xxplore.shared.util.URIHelper;
 import org.ateam.xxplore.core.ExploreEnvironment;
 import org.ateam.xxplore.core.model.definition.ModelDefinition;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.openrdf.repository.RepositoryException;
 import org.xmedia.accessknow.sesame.persistence.ConnectionProvider;
 import org.xmedia.accessknow.sesame.persistence.ExtendedSesameDaoManager;
-import org.xmedia.accessknow.sesame.persistence.SesameRepositoryFactory;
+import org.xmedia.accessknow.sesame.persistence.SesameConnection;
 import org.xmedia.accessknow.sesame.persistence.SesameSession;
 import org.xmedia.accessknow.sesame.persistence.SesameSessionFactory;
 import org.xmedia.oms.adapter.kaon2.persistence.Kaon2ConnectionProvider;
@@ -132,44 +131,77 @@ public class XXPloreCommandline {
 		private void prepareRepository(){
 			
 			Emergency.checkPrecondition(State.CURRENT_STATE == State.PARAMETERS_LOADED, "Parameters not loaded yet. " +
-					"\nPlease do so before preparing repository.");
-			
+			"\nPlease do so before preparing repository.");
+
+
 			//create conncetion provider
 			IConnectionProvider provider = null;
-			String providerClass;
-			
-			if(m_parameters.get(KbEnvironment.CONNECTION_PROVIDER_CLASS) != null){
-				providerClass = m_parameters.getProperty(KbEnvironment.CONNECTION_PROVIDER_CLASS);
+			String providerClazz = m_parameters.getProperty(KbEnvironment.CONNECTION_PROVIDER_CLASS);
+			if(providerClazz == null) {
+				providerClazz = KbEnvironment.DEFAULT_CONNECTION_PROVIDER_CLASS;
+			}
+
+			//TODO better do class loading
+			try {
+				provider = (IConnectionProvider)Class.forName(providerClazz).newInstance();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			provider.configure(m_parameters);
+
+			//				set resource and index locations ...
+			if(m_parameters.containsKey(ExploreEnvironment.RESOURCE_LOCATION)){
+				String location = m_parameters.getProperty(ExploreEnvironment.RESOURCE_LOCATION);
+				ExploreEnvironment.LocationHelper.setResourceLocation(location);
 			}
 			else{
-				providerClass = KbEnvironment.DEFAULT_CONNECTION_PROVIDER_CLASS;
+				//					default value
+				String location = ResourcesPlugin.getWorkspace().getRoot().getLocation().removeLastSegments(1).toString()
+				+ ExploreEnvironment.DEFAULT_RESOURCE_LOCATION_SUFFIX;
+				ExploreEnvironment.LocationHelper.setResourceLocation(location);
+			}
+			if(m_parameters.containsKey(ExploreEnvironment.INDEX_LOCATION)){
+				String location = m_parameters.getProperty(ExploreEnvironment.INDEX_LOCATION);
+				ExploreEnvironment.LocationHelper.setIndexLocation(location);
+			}
+			else{
+				//					default value
+				String location = ResourcesPlugin.getWorkspace().getRoot().getLocation().removeLastSegments(1).toString()
+				+ ExploreEnvironment.DEFAULT_RESOURCE_LOCATION_SUFFIX;
+				ExploreEnvironment.LocationHelper.setResourceLocation(location);
 			}
 
-			try {
-				provider = (IConnectionProvider)Class.forName(providerClass).newInstance();
-			} 
-			catch (Exception e) {
-				IStatus status = new MultiStatus(ExplorePlugin.PLUGIN_ID,
-						ExplorePlugin.LOADING_DATASOURCE_ERROR,"The definition could not be correctly loaded",e);
-				try {
-					throw new CoreException(status);
-				} 
-				catch (CoreException e1) {
-					e1.printStackTrace();
-				}
-			}
-
-			provider.configure(m_parameters);		
-			
+			//load ontology	
 			IOntology onto = null;
+			SesameConnection ses_con = null;
 			try {
-				if (provider instanceof ConnectionProvider){
-					onto = provider.getConnection().loadOrCreateOntology(PropertyUtils.convertToMap(m_parameters));
-				}
-				else if (provider instanceof Kaon2ConnectionProvider){
+				if (provider instanceof ConnectionProvider) {
+
+					try {
+						ses_con = new SesameConnection(ExploreEnvironment.LocationHelper.getResourceLocation());
+					} catch (RepositoryException e2) {
+						e2.printStackTrace();
+						ses_con = (SesameConnection)provider.getConnection();
+					}	
+
+					try {
+						onto = ses_con.loadOntology(PropertyUtils.convertToMap(m_parameters));
+					}
+					catch (OntologyLoadException e) {
+
+						onto = ses_con.createOntology(PropertyUtils.convertToMap(m_parameters));
+
+						try {
+							addFileToRepository(onto,PropertyUtils.convertToMap(m_parameters));
+						} catch (MissingParameterException e1) {
+							e1.printStackTrace();
+						}
+					}
+
+				} else if (provider instanceof Kaon2ConnectionProvider) {
 					onto = provider.getConnection().loadOntology(PropertyUtils.convertToMap(m_parameters));
 				}
-
 			} catch (DatasourceException e) {
 				e.printStackTrace();
 			} catch (MissingParameterException e) {
@@ -178,96 +210,72 @@ public class XXPloreCommandline {
 				e.printStackTrace();
 			} catch (OntologyCreationException e) {
 				e.printStackTrace();
-			}catch (OntologyLoadException e) {
+			} catch (OntologyLoadException e) {
 				e.printStackTrace();
 			}
 
 			if (provider instanceof ConnectionProvider) {
 
-				try {
-					addFileToRepository(onto,PropertyUtils.convertToMap(m_parameters));
-				} catch (MissingParameterException e1) {
-					e1.printStackTrace();
-				}
 				SesameSessionFactory sesame_factory = new SesameSessionFactory(new XMURIFactoryInsulated());
 				ISession session = null;
 
 				try {
-					session = sesame_factory.openSession(provider.getConnection(), onto);
+					session = sesame_factory.openSession(ses_con, onto);
 				} catch (DatasourceException e) {
 					e.printStackTrace();
 				} catch (OpenSessionException e) {
 					e.printStackTrace();
 				}
-				
 				//set dao manager
 				PersistenceUtil.setDaoManager(ExtendedSesameDaoManager.getInstance((SesameSession)session));
+
 				session.close();			
 			}
 			else if (provider instanceof Kaon2ConnectionProvider) {
-				
 				ISessionFactory factory = SessionFactory.getInstance();
 				factory.configure(PropertyUtils.convertToMap(m_parameters));
+
 				PersistenceUtil.setDaoManager(Kaon2DaoManager.getInstance());
-				
 			}
 
 			ISessionFactory factory = SessionFactory.getInstance();
 			PersistenceUtil.setSessionFactory(factory); 
 			//open a new session with the ontology
 			try {
-				factory.openSession(provider.getConnection(),onto);
+				factory.openSession(ses_con,onto);
 			} catch (DatasourceException e) {
 				e.printStackTrace();
 			} catch (OpenSessionException e) {
 				e.printStackTrace();
 			}
-			
+
+			m_onto =  onto;
+
 			State.CURRENT_STATE = State.REPOSITORY_READY;
-			
-			m_onto = onto;
 		}
 		
 		private void addFileToRepository(IOntology onto, Map<String, Object> parameters)throws MissingParameterException{
 			
 			if(!parameters.containsKey(ExploreEnvironment.ONTOLOGY_FILE_PATH)) {
-				throw new MissingParameterException("Ontology file path is missing.");
+				throw new MissingParameterException(ExploreEnvironment.ONTOLOGY_FILE_PATH+" missing!");
 			}
 			
 			if(!parameters.containsKey(ExploreEnvironment.ONTOLOGY_FILE_NAME)) {
-				throw new MissingParameterException("Ontology filename is missing.");
+				throw new MissingParameterException(ExploreEnvironment.ONTOLOGY_FILE_NAME+" missing!");
 			}
 			
 			if(!parameters.containsKey(ExploreEnvironment.BASE_ONTOLOGY_URI)) {
-				throw new MissingParameterException("Ontology base-uri is missing.");
+				throw new MissingParameterException(ExploreEnvironment.BASE_ONTOLOGY_URI+" missing!");
 			}
 			
 			if(!parameters.containsKey(ExploreEnvironment.LANGUAGE)) {
-				throw new MissingParameterException("Ontology language is missing.");
-			}
-			
-//			Check whether file has already been added to repository 
-			
-			File repositoryDir = new File(SesameRepositoryFactory.REPO_PATH_DEFAULT);
-			String path = repositoryDir.getAbsolutePath()+"/"+parameters.get(ExploreEnvironment.ONTOLOGY_FILE_NAME)+"_added";
-			boolean already_added = Arrays.asList(repositoryDir.list()).contains(parameters.get(ExploreEnvironment.ONTOLOGY_FILE_NAME)+"_added");
-			
-			if(already_added) {
-				return;
-			} else{
-				try {
-					(new File(path)).createNewFile();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+				throw new MissingParameterException(ExploreEnvironment.LANGUAGE+" missing!");
 			}
 			
 			String filePath = (String)parameters.get(ExploreEnvironment.ONTOLOGY_FILE_PATH);
 			String baseUri = (String)parameters.get(ExploreEnvironment.BASE_ONTOLOGY_URI);
 			String language = (String)parameters.get(ExploreEnvironment.LANGUAGE);
-						
-//			File has not been added -> add it now
-			
+					
 			try {
 				onto.importOntology(language, baseUri, new FileReader(filePath));
 			} 
