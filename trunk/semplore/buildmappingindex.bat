870c9295e38caa9a8601b53e44e3fcd6af6c04f1
xiaoruoruo <xiaoruoruo@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-09-16 08:06:53 +0000

    add datasrc.cfg

--- trunk/semplore/buildmappingindex.bat
+++ trunk/semplore/buildmappingindex.bat
@@ -1,19 +1,17 @@
 @echo %1: input mapping data(two columns, first column is uri in ds1, second's is in ds2) 
-@echo %2: path of index of ds1
-@echo %3: path of index of ds2
+@echo %2: name of ds1
+@echo %3: name of ds2
 @echo %4: strategy, usually 3
-@echo %5: name of ds1
-@echo %6: name of ds2
  
 java -cp bin Hashify < %1 > mapping.hash
 @rem only need sorting when using lucene index
 sort -g -k 1 mapping.hash > mapping.sort 
 
-cat mapping.sort | java -cp bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.btc.mapping.MappingIndexPreparer %2 %3 %4 > mapping.prepared
-sort -g -k 1 mapping.prepared > %5_%6
-sort -g -k 2 mapping.prepared | gawk "BEGIN {OFS=\"\t\"} {print $2,$1}" > %6_%5
-cat %5_%6 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %5_%6_index.head %5_%6_index.map
-cat %6_%5 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %6_%5_index.head %6_%5_index.map
+cat mapping.sort | java -cp bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.btc.mapping.MappingIndexPreparer config\datasrc.cfg %2 %3 %4 > mapping.prepared
+sort -g -k 1 mapping.prepared > %2_%3
+sort -g -k 2 mapping.prepared | gawk "BEGIN {OFS=\"\t\"} {print $2,$1}" > %3_%2
+cat %2_%3 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %2_%3_index.head %2_%3_index.map
+cat %3_%2 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %3_%2_index.head %3_%2_index.map
 
 @rem cleanup
-del mapping.hash mapping.sort mapping.prepared %5_%6 %6_%5
+del mapping.hash mapping.sort mapping.prepared %2_%3 %3_%2
