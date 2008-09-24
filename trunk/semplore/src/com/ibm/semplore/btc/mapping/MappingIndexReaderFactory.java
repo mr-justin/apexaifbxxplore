db9e020b8aa977d172dc18c19827204737b5a615
xiaoruoruo <xiaoruoruo@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-09-24 08:14:40 +0000

    MappingIndexReader

---
+++ trunk/semplore/src/com/ibm/semplore/btc/mapping/MappingIndexReaderFactory.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.ibm.semplore.btc.mapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author xrsun
+ *
+ */
+public class MappingIndexReaderFactory {
+	private static File mappingIndexLoc;
+	
+	private static HashMap<String, MappingIndexReader> readerPool;
+	
+	public static void init(File _mappingIndexLoc) {
+		mappingIndexLoc = _mappingIndexLoc;
+	}
+	
+	public static MappingIndexReader getMappingIndexReader(String file) throws IOException {
+		MappingIndexReader reader = readerPool.get(file);
+		if (reader == null) {
+			reader = new MappingIndexReader(mappingIndexLoc.getPath() + File.separatorChar + file);
+			readerPool.put(file, reader);
+		}
+		return reader;
+	}
+
+}
