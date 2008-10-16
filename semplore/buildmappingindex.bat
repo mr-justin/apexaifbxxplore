59bf4d07f0ef7724de615a6405e087c1effce345
xiaoruoruo <xiaoruoruo@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-10-16 08:02:59 +0000

    gaea->apollo

--- semplore/buildmappingindex.bat
+++ semplore/buildmappingindex.bat
@@ -5,13 +5,13 @@
  
 java -cp bin Hashify < %1 > mapping.hash
 @rem only need sorting when using lucene index
-sort -g -k 1 mapping.hash | uniq > mapping.sort 
+sort -g -k 1 -u mapping.hash > mapping.sort 
 
 cat mapping.sort | java -cp bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.btc.mapping.MappingIndexPreparer config\datasrc.cfg %2 %3 %4 > mapping.prepared
 sort -g -k 1 mapping.prepared > %2_%3
 sort -g -k 2 mapping.prepared | gawk "BEGIN {OFS=\"\t\"} {print $2,$1}" > %3_%2
-cat %2_%3 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %2_%3_index.head %2_%3_index.map
-cat %3_%2 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %3_%2_index.head %3_%2_index.map
+cat %2_%3 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %2_%3_index.head %2_%3_index.map 0
+cat %3_%2 | java -cp bin com.ibm.semplore.btc.mapping.MappingIndexBuilder %3_%2_index.head %3_%2_index.map 0
 
 @rem cleanup
 del mapping.hash mapping.sort mapping.prepared %2_%3 %3_%2
