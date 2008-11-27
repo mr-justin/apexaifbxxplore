f59c3b89a0e1c9b7af9ac410c907073bf175122e
xiaoruoruo <xiaoruoruo@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-11-27 08:44:59 +0000

    numeric unique sort util

--- semplore/load.bat
+++ semplore/load.bat
@@ -5,8 +5,9 @@ path = C:\Program Files\Java\jre1.6.0_07\bin;c:\cygwin\bin;%PATH%
 
 java -Xmx2g -cp ./bin;./lib/lucene-core-2.3.2.jar;./lib/dsiutils-1.0.5.jar;./lib/fastutil5-5.1.4.jar;./lib/jakarta-commons-httpclient-3.0.1.jar;./lib/jakarta-commons-lang-2.3.jar;./lib/jakarta-commons-logging-1.1.jar;./lib/law-1.4.jar;./lib/log4j-1.2.14.jar;./lib/openrdf-sesame-2.2-beta2-onejar.jar;./lib/tar.jar; com.ibm.semplore.test.TestLoad config\%1.cfg
 
-dir %2\hashdata
-sort -n -S 512m -T . --compress-prog=gzip %2\hashdata | uniq | tee %2\hashdata.sort | wc -l
+dir %2/hashdata
+sh sortun.sh %2/hashdata | tee %2/hashdata.sort | wc -l
+@rem sort -n -S 512m -T . --compress-prog=gzip %2\hashdata | uniq | tee %2\hashdata.sort | wc -l
 
 java -Xmx3g -cp ./bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.test.TestIndex config\%1.cfg
 
--- semplore/test.bat
+++ semplore/test.bat
@@ -2,7 +2,8 @@ path=c:\cygwin\bin;%path%
 
 java -cp ./bin;./lib/lucene-core-2.3.2.jar;./lib/dsiutils-1.0.5.jar;./lib/fastutil5-5.1.4.jar;./lib/jakarta-commons-httpclient-3.0.1.jar;./lib/jakarta-commons-lang-2.3.jar;./lib/jakarta-commons-logging-1.1.jar;./lib/law-1.4.jar;./lib/log4j-1.2.14.jar;./lib/openrdf-sesame-2.2-beta2-onejar.jar;./lib/tar.jar; com.ibm.semplore.test.TestLoad config\test.cfg
 
-sort -n -S 512m -T . --compress-prog=gzip test\hashdata | uniq | tee test\hashdata.sort | wc -l
+sh sortun.sh test/hashdata | tee test\hashdata.sort | wc -l
+@rem sort -n -S 512m -T . test\hashdata | uniq | tee test\hashdata.sort | wc -l
 
 java -cp ./bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.test.TestIndex config\test.cfg
 
@@ -10,5 +11,9 @@ java -cp ./bin;./lib/lucene-core-2.3.2.jar com.ibm.semplore.test.TestIndex confi
 
 @rem java -Xmx1g -cp ./bin;./lib/je-3.3.69.jar com.ibm.semplore.btc.BuildSnippetDB snippet\%1 data.nt
 
-java -cp bin;lib\lucene-core-2.3.2.jar;lib\je-3.3.69.jar;lib\log4j-1.2.14.jar com.ibm.semplore.test.TestEvaluator config\datasrc.cfg test\query_graph.txt
+java -cp bin;lib\lucene-core-2.3.2.jar;lib\je-3.3.69.jar;lib\log4j-1.2.14.jar com.ibm.semplore.test.TestEvaluator test\query_rudi.txt > result
+diff result test\ans_rudi.txt
+
+java -cp bin;lib\lucene-core-2.3.2.jar;lib\je-3.3.69.jar;lib\log4j-1.2.14.jar com.ibm.semplore.test.TestEvaluator test\query_shanghai.txt > result
+diff result test\ans_shanghai.txt
 
