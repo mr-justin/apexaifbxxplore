2882b25f96e35d0bb44d7141d34a9d8545ab8204
coolfishchen <coolfishchen@4f1b21da-4c4f-0410-9e14-fba73de220fd>
2008-11-29 12:04:11 +0000

    All the Parameters load from config file is here

---
+++ trunk/q2semantic/src/org/team/xxplore/core/service/q2semantic/Parameters.java
@@ -0,0 +1,73 @@
+package org.team.xxplore.core.service.q2semantic;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+public class Parameters {
+	
+	public static Parameters single = null;
+	public static String configFilePath = null;
+	
+	public double DEFAULT_SCORE = 2.0;
+	public double EDGE_SCORE = 0.5;
+	
+	public static void setConfigFilePath(String fn) {
+		configFilePath = fn;
+	}
+	
+	public static Parameters getParameters() {
+		if(single == null) {
+			single = new Parameters(configFilePath);
+		}
+		return single;
+	}
+	
+	public String root;
+	public String summaryObjsRoot;
+	public String keywordIndexRoot;
+	public String mappingIndexRoot;
+	
+	public HashSet<String> keywordIndexSet;
+	public HashMap<String, String> summaryObjSet;
+	
+	public Set<String> getDataSourceSet() {
+		return summaryObjSet.keySet();
+	}
+	
+	private Parameters(String fn) {
+		loadPara(fn);
+	}
+	
+	private void loadPara(String fn) {
+		try {
+			Properties prop = new Properties();
+			InputStream is = new FileInputStream(fn);
+			prop.load(is);
+			root = prop.getProperty("root")+File.separator;
+			summaryObjsRoot = root+prop.getProperty("summaryObjsRoot")+File.separator;
+			keywordIndexRoot = root+prop.getProperty("keywordIndexRoot")+File.separator;
+			mappingIndexRoot = root+prop.getProperty("mappingIndexRoot")+File.separator;
+			System.out.println("Root:"+root+"\r\nsummaryObjsRoot:"+summaryObjsRoot+"\r\nkeywordIndexRoot:"+keywordIndexRoot+"\r\nmappingIndexRoot:"+mappingIndexRoot);
+
+			//		add keywordindexes
+			keywordIndexSet = new HashSet<String>();
+			File[] indexes = new File(keywordIndexRoot).listFiles();
+			for(File index: indexes)
+				keywordIndexSet.add(index.getAbsolutePath());
+
+			//		add graphs
+			summaryObjSet = new HashMap<String, String>();
+			File[] summaries = new File(summaryObjsRoot).listFiles();
+			for(File summary: summaries)
+				summaryObjSet.put(summary.getName().substring(0, summary.getName().lastIndexOf('-')), summary.getAbsolutePath());		
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
